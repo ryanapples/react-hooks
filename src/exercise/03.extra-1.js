24c8459cf7e80a28e45f6bb10aacb3f66156cd92
Ryan Apellanes <ryanapellanes.dev@gmail.com>
2022-03-02 16:25:17 -0800

    exercise 3 - complete

---
+++ src/exercise/03.extra-1.js
@@ -0,0 +1,50 @@
+// Lifting state
+// http://localhost:3000/isolated/exercise/03.js
+
+import * as React from 'react'
+
+function Name() {
+  const [name, setName] = React.useState('')
+  return (
+    <div>
+      <label htmlFor="name">Name: </label>
+      <input
+        id="name"
+        value={name}
+        onChange={event => setName(event.target.value)}
+      />
+    </div>
+  )
+}
+
+// 🐨 accept `animal` and `onAnimalChange` props to this component
+function FavoriteAnimal({animal, onAnimalChange}) {
+  return (
+    <div>
+      <label htmlFor="animal">Favorite Animal: </label>
+      <input id="animal" value={animal} onChange={onAnimalChange} />
+    </div>
+  )
+}
+
+function Display({animal}) {
+  return <div>{`Your favorite animal is: ${animal}!`}</div>
+}
+
+function App() {
+  const [animal, setAnimal] = React.useState('')
+  return (
+    <form>
+      <Name />
+      {/* 🐨 pass the animal and onAnimalChange prop here (similar to the Name component above) */}
+      <FavoriteAnimal
+        animal={animal}
+        onAnimalChange={event => setAnimal(event.target.value)}
+      />
+      {/* 🐨 pass the animal prop here */}
+      <Display animal={animal} />
+    </form>
+  )
+}
+
+export default App
